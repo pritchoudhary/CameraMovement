cc87dd463c264716dd14b86ca7d165590844c791
Pritish Choudhary <pritish.choudhary@yahoo.in>
2016-07-02 16:09:46 +1000

    Added a camera follow system and a basic movement system. Bugs are still present. Need to FIX.

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject target;
+    public float damping = 1;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = target.transform.position - transform.position;
+    }
+
+    //To orient the camera behind the target
+    void LateUpdate()
+    {
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = target.transform.eulerAngles.y;
+        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
+
+        // to orient the offset the same as the target
+        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+        transform.position = target.transform.position - (rotation * offset);
+
+        //To keep looking at the player
+        transform.LookAt(target.transform);
+    }
+
+}
+
---
+++ Assets/Movement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float moveSpeed = 10;
+    public float turnSpeed = 50;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        transform.Rotate(0, horizontal, 0);
+
+        float vertical = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Translate(0, 0, vertical);
+    }
+}
