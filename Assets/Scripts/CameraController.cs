e19e568284576d18d0a06d697bede809265eac27
Marivan Ebrahimi <1003873@student.sae.edu.au>
2016-07-10 21:57:59 +1000

    updated code with distance check and comments.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -228,8 +228,6 @@ public class CameraController : MonoBehaviour {
         {
             position.distanceFromTarget = position.minZoom;
         }
-
-
     }
 
 
--- Assets/Scripts/CameraSwitcher.cs
+++ Assets/Scripts/CameraSwitcher.cs
@@ -1,37 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraSwitcher : MonoBehaviour
+public class CameraSwitcher : MonoBehaviour // wrong name // should be named Camera Controller
 {
-    public Camera FPSCamera;
+    public Camera FPCamera;
     public Camera TPCamera;
 
-    private bool camSwitch = false;
+    private float minZoom = 5;
+    private float maxZoom = 15;
+    public float zoomSpeed = 50;
+
+    [Tooltip("true = TP, false = FP")]
+    private bool TPCamActive = false;
 
 	// Use this for initialization
 	void Start ()
     {
-        FPSCamera.gameObject.SetActive(true);
+        // Enable first person camera at start and disable third person
+        FPCamera.gameObject.SetActive(true);
         TPCamera.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	    if (Input.GetKeyDown(KeyCode.C))
+        // Switch cam with "F"
+	    if (Input.GetKeyDown(KeyCode.F))
         {
-            camSwitch = !camSwitch;
+            TPCamActive = !TPCamActive;
         }
 
-        if(camSwitch)
+        // check what cam should be enabled
+        if(TPCamActive)
         {
-            FPSCamera.gameObject.SetActive(false);
+            FPCamera.gameObject.SetActive(false);
             TPCamera.gameObject.SetActive(true);
         }
         else
         {
-            FPSCamera.gameObject.SetActive(true);
+            FPCamera.gameObject.SetActive(true);
             TPCamera.gameObject.SetActive(false);
         }
-	}
+
+        // If TPCamera is active enable scrolling (because we dont need it in FPCamera).
+        // if TPCamera is close to head -> switch to FPCamera
+        if (TPCamActive)
+        {
+            if (Vector3.Distance(TPCamera.transform.position, transform.position) > minZoom)
+            {
+                if (Input.GetAxis("Mouse ScrollWheel") > 0)
+                {
+                    TPCamera.transform.position = Vector3.MoveTowards(TPCamera.transform.position, transform.position, zoomSpeed * Time.deltaTime);
+                }
+            }
+            else
+            {
+                TPCamera.transform.position = Vector3.MoveTowards(TPCamera.transform.position, transform.position, -zoomSpeed * Time.deltaTime);
+                TPCamActive = !TPCamActive;
+            }
+            if (Vector3.Distance(TPCamera.transform.position, transform.position) < maxZoom)
+            {
+                if (Input.GetAxis("Mouse ScrollWheel") < 0)
+                {
+                    TPCamera.transform.position = Vector3.MoveTowards(TPCamera.transform.position, transform.position, -zoomSpeed * Time.deltaTime);
+                }
+            }
+            else
+            {
+                TPCamera.transform.position = Vector3.MoveTowards(TPCamera.transform.position, transform.position, zoomSpeed * Time.deltaTime);
+            }
+        }
+    }
 }
